343ec032af40bb418cbcb758ac1155b0daac52d6
elisakaisa <elisa.k.perini@gmail.com>
2022-05-30 15:35:48 +0200

    init

---
+++ part1/example/src/App.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
+// always needed to work!!!!!!
+export default App
+
