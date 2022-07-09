7ab1bf29169d8c99274045d558aa2e6963090f3a
elisakaisa <elisa.k.perini@gmail.com>
2022-07-09 15:33:57 +0200

    part 8b done

---
+++ part8/library/library-frontend/src/queries.js
@@ -0,0 +1,52 @@
+import { gql } from '@apollo/client'
+
+export const ALL_BOOKS = gql`
+query {
+  allBooks {
+    title
+    published
+    author
+    id
+  }
+}
+`
+
+export const ALL_AUTHORS = gql`
+query {
+  allAuthors {
+    name
+    id
+    born
+    bookCount
+  }
+}
+`
+
+
+export const CREATE_BOOK = gql`
+mutation createBook(
+  $title: String!
+  $author: String!
+  $published: Int!
+  $genres: [String!]!
+) {
+  addBook(title: $title, author: $author, published: $published, genres: $genres) {
+    title
+    author
+    published
+  }
+}
+`
+
+export const EDIT_BIRTH_YEAR = gql`
+  mutation editBirthYear ($name: String!, $setBornTo: Int!) {
+    editAuthor(
+      name: $name,
+      setBornTo: $setBornTo
+    ) {
+      name
+      born
+      bookCount
+    }
+  }
+`
